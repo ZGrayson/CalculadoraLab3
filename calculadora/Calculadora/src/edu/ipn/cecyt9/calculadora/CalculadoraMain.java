b09e5b0cf3d009c93f9bfa8fca087348d571e608
ZGrayson <30833439+ZGrayson@users.noreply.github.com>
2017-08-15 08:59:20 -0500

    Agregadas 5 operaciones nuevas

---
+++ calculadora/Calculadora/src/edu/ipn/cecyt9/calculadora/CalculadoraMain.java
@@ -0,0 +1,25 @@
+
+package edu.ipn.cecyt9.calculadora;
+
+/**
+ * Ejercicio: Calculadora basica 
+ * Implementar la Interfaz de usuario 
+ * Implementar los metodos siguientes a través de los listeners adecuados: 
+ * - Sumar 
+ * - Restar 
+ * - Multiplicar 
+ * - Dividir
+ * 
+ * Interfaz para nuestra calculadora basica
+ * 
+ * @author: Diaz García Zyanya Paola
+ * @author: Martínez Ortíz Fabiola Yahel
+ * @version:  1.1 
+ */
+
+public class CalculadoraMain {
+ public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+	}   
+}
